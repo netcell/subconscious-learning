9927b219c8f05499353633da71831df1f3591169
Nguyen Tuan Anh <tebaomang@Nguyens-MacBook-Pro-2.local>
2016-04-09 22:23:43 +0700

    update main page

---
+++ app/src/js/scripts/main.js
@@ -0,0 +1,22 @@
+var $ = require('jquery');
+require('jquery-ui');
+require('./jquery.transform');
+var AnimMan = require('./AnimationManager');
+var AnimEl = require('./AnimationElement');
+
+module.exports = function() {
+	var animation = new AnimMan([
+		[
+			new AnimEl('body', {
+				'opacity' : 1
+			})
+		]
+	]);
+	animation.show().then(function() {
+		$('#dreamoff').one('click', function() {
+			animation.hide().then(function() {
+				window.location = "/level1.html";
+			})
+		});
+	})
+}
